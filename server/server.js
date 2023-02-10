b9b3be4eac6c46fed4bb68241fc19048f6a0504f
Wilhelm <wasander13@gmail.com>
2023-02-10 11:20:09 +0100

    Added a connection to my database on MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
+require('dotenv/config');
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    mongoose.set('strictQuery', true), 
+    () => console.log("Connected to Database!")
+);
 
 app.get("/api", (req, res) => {
     res.json({"users": ["UserOne", "UserTwo", "UseThree","UserFour"]})
 });
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(5000, () => {console.log("Server started on port 5000")});
